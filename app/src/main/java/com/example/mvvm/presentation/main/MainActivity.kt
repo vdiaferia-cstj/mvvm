ec86785613dd564aade2aff35ec2585d377793a6
Vincent Diaferia <1967480@cstj.qc.ca>
2023-02-09 11:06:34 -0500

    Fin du cours de jeudi, fin animation

--- app/src/main/java/com/example/mvvm/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/presentation/main/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.mvvm.presentation.main
 
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnticipateOvershootInterpolator
+import android.view.animation.LinearInterpolator
 import androidx.activity.viewModels
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.lifecycleScope
 import com.example.mvvm.R
+import com.example.mvvm.core.Constants
 import com.example.mvvm.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.flow.launchIn
@@ -15,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel : MainViewModel by viewModels()
 
+    private  lateinit var rocketAnimation: ValueAnimator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,21 +32,22 @@ class MainActivity : AppCompatActivity() {
         //À chaque fois qu'un nouveau state est reçu
         viewModel.mainUiState.onEach {
 
-            when(it) {
+            when (it) {
                 MainUiState.Empty -> Unit
                 is MainUiState.Error -> {
-                    Snackbar.make(binding.root, "", Snackbar.LENGTH_INDEFINITE).setAction(R.string.recharge){
-                        viewModel.recharge()
-                    }.show()
+                    Snackbar.make(binding.root, "", Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.recharge) {
+                            viewModel.recharge()
+                        }.show()
                 }
                 MainUiState.Loading -> {
-                binding.btnStart.isEnabled = false
-
+                    binding.btnStart.isEnabled = false
+                    rocketAnimation.start()
                 }
                 is MainUiState.Success -> {
                     with(binding) {
                         txvPilotName.text = it.pilot.name
-                        txvLevel.text = getString(R.string.level,it.pilot.level)
+                        txvLevel.text = getString(R.string.level, it.pilot.level)
 
                         txvLife.text = it.pilot.life.toString()
                         txvEnergy.text = it.pilot.energy.toString()
@@ -55,10 +62,38 @@ class MainActivity : AppCompatActivity() {
 
         }.launchIn(lifecycleScope)
 
-    binding.btnStart.setOnClickListener {
-        val revolution = binding.sldRevolution.value.toInt()
-        val isTraining = binding.swtTraining.isChecked
-        viewModel.fly(revolution, isTraining)
+        binding.btnStart.setOnClickListener {
+            val revolution = binding.sldRevolution.value.toInt()
+            val isTraining = binding.swtTraining.isChecked
+
+            createAnimation()
+            viewModel.fly(revolution, isTraining)
+        }
+
+
+
     }
+
+
+
+    private fun createAnimation(){
+
+        val layoutParams = binding.imvRocket.layoutParams as ConstraintLayout.LayoutParams
+        val repeatCount = binding.sldRevolution.value.toInt()
+        val startAngle = layoutParams.circleAngle
+        val endAngle = startAngle - 360
+
+        rocketAnimation = ValueAnimator.ofFloat(startAngle, endAngle)
+        rocketAnimation.repeatCount = repeatCount - 1
+        rocketAnimation.duration = Constants.REVOLUTION_DURATION
+        rocketAnimation.interpolator =  AnticipateOvershootInterpolator() //TODO: Revenir Interpolation
+
+
+        rocketAnimation.addUpdateListener {
+            val animatedValue = it.animatedValue as Float
+            layoutParams.circleAngle = animatedValue
+            binding.imvRocket.layoutParams = layoutParams
+            binding.imvRocket.rotation = animatedValue - 90
+        }
     }
 }
